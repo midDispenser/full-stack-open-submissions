ab5a1d21d0dae62dcd5e5ae607e8dc31a3b06d88
midDispenser <198863769+midDispenser@users.noreply.github.com>
2025-02-12 00:49:59 -0600

    excercise 1.1 done

--- part1/course-information/src/App.jsx
+++ part1/course-information/src/App.jsx
@@ -1,27 +1,45 @@
+const Header = ({title}) => {
+    return (
+        <h1> {title} </h1>
+    );
+};
+
+const Contents = ({subjects, counters}) => {
+
+    return (
+        <div>
+            <p> {subjects[0]} {counters[0]} </p>
+            <p> {subjects[1]} {counters[1]} </p>
+            <p> {subjects[2]} {counters[2]} </p>
+        </div>
+    );
+};
+
+const Total = ({sum}) => {
+
+    return (
+        <p>Number of exercises {sum} </p>
+    );
+};
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+    const course = 'Half Stack application development';
+    const part1 = 'Fundamentals of React';
+    const exercises1 = 10;
+    const part2 = 'Using props to pass data';
+    const exercises2 = 7;
+    const part3 = 'State of a component';
+    const exercises3 = 14;
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        <Header title={course}/>
+        <Contents
+            subjects={[part1, part2, part3]}
+            counters={[exercises1, exercises2, exercises3]} />
+        <Total sum={exercises1 + exercises2 + exercises3} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
