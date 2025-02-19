644cc2b1c35f1b59f097b2be0aa162d934910cd9
midDispenser <198863769+midDispenser@users.noreply.github.com>
2025-02-19 06:20:34 -0600

    excercise 2.8 done

--- part2/the-phonebook/src/App.jsx
+++ part2/the-phonebook/src/App.jsx
@@ -4,37 +4,55 @@ function PeopleList ({ people }){
     return (
         <div>
             {people.map(p =>
-                <PersonData key={p.id} name={p.name} />
+                <PersonData key={p.id}
+                    name={p.name} phone={p.number} />
             )}
         </div>
     );
 };
 
-function PersonData ({ name }) {
-    return <p> {name}: number xdd </p>;
+function PersonData ({ name, phone }) {
+    return <p> {name}: {phone} </p>;
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([{ id:0, name: 'John Doe' }]);
+    const [persons, setPersons] = useState([{
+        id:0,
+        name: 'John Doe',
+        number:'+1 555 555'
+    }]);
+
+    const [newPhone, setNewPhone] = useState('');
     const [newName, setNewName] = useState('');
-    
-    const addName = (event) => {
+
+    const addPerson = (event) => {
         event.preventDefault();
 
-        const isDupe = persons.some((p) => p.name === newName);
-        if(isDupe) {
-            alert(`${newName} is already added to phonebook`);
+        const dupeName = persons.some((p) => p.name === newName);
+        if(dupeName) {
+            alert(`The name ${newName} is already added to phonebook`);
             setNewName('');
+            return;
+        }
+        // Im aware duplicating this means going through the array twice over,
+        // a more efficient option would be to use either a filter, a forEach
+        // or make a separate function. however, for now this is simpler for me.
+        const dupePhone = persons.some((p) => p.number === newPhone);
+        if (dupePhone) {
+            alert(`The number ${newPhone} is already added to phonebook`);
+            setNewPhone('');
             return;
         }
 
         const newPerson = {
             id: Math.floor(Math.random() * 100),
             name: newName,
+            number: newPhone,
         };
 
         setPersons(persons.concat(newPerson));
         setNewName('');
+        setNewPhone('');
     };
 
     return (
@@ -44,12 +62,18 @@ const App = () => {
                 <div>
                     <label for='nameInput'>name: </label>
                     <input name='nameInput' value={newName}
-                                 onChange={(e) => setNewName(e.target.value)} />
+                        onChange={(e) => setNewName(e.target.value)} />
+                </div>
+                <div>
+                    <label for='nameInput'>number: </label>
+                    <input name='phoneInput' value={newPhone} type="tel"
+                        onChange={(e) => setNewPhone(e.target.value)} />
                 </div>
                 <div>debug: {newName}</div>
+                <div>debug: {newPhone}</div>
 
                 <div>
-                    <button type="submit" onClick={addName}>add</button>
+                    <button type="submit" onClick={addPerson}>add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
